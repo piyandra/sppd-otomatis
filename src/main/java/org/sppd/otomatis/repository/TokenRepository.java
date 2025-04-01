0727928f104fc8535b29e90f7af916bd7898051c
piyan1212 <piyanstefani@gmail.com>
2025-04-01 09:01:02 +0700

    User Request : - Login - Logout - Get Me - Add Token Interceptor

---
+++ src/main/java/org/sppd/otomatis/repository/TokenRepository.java
@@ -0,0 +1,11 @@
+package org.sppd.otomatis.repository;
+
+import org.sppd.otomatis.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Users, String> {
+
+    boolean existsByToken(String token);
+}
